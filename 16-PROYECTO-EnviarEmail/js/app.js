0031b1c4cd6a19ed0e6aef7babb7011c730895a2
garbf1996 <garbfbatista@gmail.com>
2022-06-16 13:50:12 -0400

    enviar correo

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,15 +1,21 @@
+
 const  btnEnvair = document.querySelector('#enviar');
 const  email = document.querySelector('#email');
 const  asunto = document.querySelector('#asunto');
 const  mensaje = document.querySelector('#mensaje');
 const  formulario = document.querySelector('#enviar-mail');
+const btnreset = document.querySelector('#resetBtn');
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-appStart();
-function appStart() {
-  document.addEventListener('DOMContentLoaded', btn);
-  email.addEventListener('blur', input);
-  asunto.addEventListener('blur', input);
-  mensaje.addEventListener('blur', input);
+
+listEvent();
+function listEvent() {
+  document.addEventListener('DOMContentLoaded',btn);
+  email.addEventListener('blur', validarFormulario);
+  asunto.addEventListener('blur', validarFormulario);
+  mensaje.addEventListener('blur', validarFormulario);
+  formulario.addEventListener('submit', enviandoEmail);
+btnreset.addEventListener('click', LimpiarFormulario);
 }
 
 function btn() {
@@ -17,29 +23,84 @@ btnEnvair.disabled = true;
 btnEnvair.classList.add('opacity-50','cursor-not-allowed');
 }
 
-function input(e){
-if(e.target.value.length > 0){
-e.target.classList.add('border','border-green-900', 'radius-6')
-}else{
-  e.target.classList.add('border','border-red-500');
-  errorMesje("Los campos son obligatorios");
+
+function validarFormulario(e){
+  //Validando los campos si esta completo
+  if(e.target.value.length > 0){
+   const error = document.querySelector('p.error');
+   if(error){
+    error.remove();
+   }
+
+    e.target.classList.remove('border','border-red-500');
+  e.target.classList.add('border','border-green-500');
+  }else{
+    e.target.classList.remove('border','border-green-500');
+    e.target.classList.add('border','border-red-500');
+    mostararError('Todos los campos son obligatorios');
+  }
+   //Validando si es un coorreo
+  if(e.target.type === 'email'){
+   if(er.test(e.target.value)){
+    const error = document.querySelector('p.error');
+    if(error){
+     error.remove();
+    }
+    e.target.classList.remove('border','border-red-500');
+     e.target.classList.add('border','border-green-500');
+   }else{
+    e.target.classList.remove('border','border-green-500');
+    e.target.classList.add('border','border-red-500');
+    mostararError('Email inválido');
+   }
+}
+
+if(er.test(email.value) && asunto.value !=='' && mensaje.value !==''){
+  btnEnvair.disabled = false;
+  btnEnvair.classList.remove('opacity-50','cursor-not-allowed')
 }
 
 }
 
 
-function errorMesje(error){
+function mostararError(mensaje) {
+const mensajeError = document.createElement('p');
+mensajeError.textContent = mensaje;
+mensajeError.classList.add('border','border-red-500','background-red-100','text-red-500','p-3','mt-5', 'error','text-center');
 
- const ware = document.createElement('p');
- ware.textContent = error;
+const errores = document.querySelectorAll('.error');
 
-  ware.classList.add('border','border-red-500', 'background-red-red','text-red-500','p-3','mt-3','text-center', 'error');
-const mensaje = document.querySelectorAll('.error');
-  
-if(mensaje.length === 0){
-  formulario.appendChild(ware);
+if(errores.length ===0){
+  formulario.appendChild(mensajeError);
+}
 }
 
+
+//spinner
+
+function enviandoEmail(e){
+  e.preventDefault();
+
+
+const spinner = document.querySelector('#spinner');
+spinner.style.display = 'flex';
+
+setTimeout(()=>{
+  spinner.style.display = 'none';
+const emailEnviado = document.createElement('p');
+emailEnviado.textContent = 'Email enviado';
+emailEnviado.classList.add('border','border-green-500','text-green-500','p-5','my-10','text-center');
+formulario.insertBefore(emailEnviado,spinner);
+setTimeout(() => {
+  emailEnviado.remove();
+  LimpiarFormulario();
+}, 5000);
+},3000); 
 }
 
+function LimpiarFormulario() {
+  formulario.reset();
+  btn();
+
+}
 
