6283c703c9a4a1a9eda2281c8e409843b649e0d0
garbf1996 <garbfbatista@gmail.com>
2022-06-17 20:43:30 -0400

    Pratica

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -4,82 +4,82 @@ const carrito = document.querySelector('#carrito');
 const vaciar = document.querySelector('#vaciar-carrito');
 articulo = [];
 
-addEvebt();
-function addEvebt(){
-lista_curso.addEventListener('click', click);
-carrito.addEventListener('click', eliminar);
-vaciar.addEventListener('click', ()=>{
+
+//funcion con eventos
+listEvente();
+function listEvente(){
+lista_curso.addEventListener('click',click);
+carrito.addEventListener('click',eliminar);
+vaciar.addEventListener('click',()=>{
     articulo = [];
+    carritoHTML();
 });
 }
-
-function eliminar(e){
-
-if(e.target.classList.contains('borrar-curso')){
-    const cursoID = e.target.getAttribute('data_id');
-    articulo = articulo.filter(curso => curso.id !== cursoID);
-}
-carritoHTML();
-}
-
 
 function click(e){
     e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-    const selec_curso = e.target.parentElement.parentElement;
-   leeDatos(selec_curso);
-    }
+if(e.target.classList.contains('agregar-carrito')){
+const selec_cursor = e.target.parentElement.parentElement;
+LeerDatos(selec_cursor);
+}
 }
 
-function leeDatos(curso){
-
-inforCursor ={
-img:curso.querySelector('img').src,
-titulo:curso.querySelector('h4').textContent,
-precio:curso.querySelector('.precio span').textContent,
-id:curso.querySelector('a').getAttribute('data-id'),
-cantidas: 1
-
+function LeerDatos(curso){
+infor = {
+    img:curso.querySelector('img').src,
+    titulo:curso.querySelector('h4').textContent,
+    precio:curso.querySelector('.precio span').textContent,
+    id:curso.querySelector('a').getAttribute('data-id'),
+    cantidad : 1
 }
 
-const exicte = articulo.some(curso=> curso.id === inforCursor.id);
+const exicte = articulo.some(curso=>curso.id === infor.id);
+
 if(exicte){
 const cursos = articulo.map(curso=>{
-if(curso.id === inforCursor.id){
- curso.cantidas ++;
- return curso;   
+if(curso.id === infor.id){
+curso.cantidad ++;
+return curso;
 }else{
-    return curso;
+return curso;
 }
-articulo= [...cursos];
+carrito = [...cursos];
 });
 }else{
 
-articulo= [...articulo,inforCursor];
+articulo = [...articulo,infor];
 }
 carritoHTML();
-
 }
+
+
 
 function carritoHTML(){
-limpiarHTML();
-    articulo.forEach(curso=>{
-        const row = document.createElement('tr');
-           row.innerHTML = ` 
-           <td><img src="${curso.img}" width="100"></td>
-           <td>${curso.titulo}</td>
-           <td>${curso.precio}</td>
-           <td>${curso.cantidas}</td>
-           <td><a href="#"  data_id="${curso.id}" class="borrar-curso">x</a></td>
-           `;
-           lista_carrito.appendChild(row);
-    });
-
+    limpairHTML();
+articulo.forEach(curso => {
+ const row = document.createElement('tr');
+ row.innerHTML = `
+<td><img src="${curso.img}" width="100"></td>
+<td>${curso.titulo}</td>
+<td>${curso.precio}</td>
+<td>${curso.cantidad}</td>
+<td> <a href="#"  class="borrar-curso" data-id="${curso.id}"</a>x</td>
+ `
+ lista_carrito.appendChild(row);
+});
 }
 
+function limpairHTML(){
+while (lista_carrito.firstChild) {
+    lista_carrito.removeChild(lista_carrito.firstChild);
+}
+}
 
-function limpiarHTML(){
-    while (lista_carrito.firstChild){
-     lista_carrito.removeChild(lista_carrito.firstChild);
-    }
+function eliminar(e){
+if(e.target.classList.contains('borrar-curso')){
+const cursoID = e.target.getAttribute('data-id');
+articulo = articulo.filter(curso =>curso.id !== cursoID);
+
+}
+carritoHTML();
 }
