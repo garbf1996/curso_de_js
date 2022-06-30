5d1b7a2450d06d02deb3b5da2dada7f616b86cdd
garbf1996 <garbfbatista@gmail.com>
2022-06-30 14:42:22 -0400

    local Storage

---
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -0,0 +1,72 @@
+const lista_tweets  = document.querySelector('#lista-tweets');
+const formulario = document.querySelector('#formulario');
+
+let tweets = [];
+
+eventos();
+function eventos(){
+     formulario.addEventListener('submit', enviamdoTweets);
+      document.addEventListener('DOMContentLoaded', ()=>{
+       tweets = JSON.parse(localStorage.getItem('tweets')); 
+       console.log(tweets);
+       tweetsHTML();
+     })
+}
+
+
+function enviamdoTweets(e){
+e.preventDefault();
+const tweet = document.querySelector('#tweet').value;
+if(tweet === ''){
+MostrarError('No tweet found');
+ return;
+}
+
+const tweetObj = {
+    id:Date.now(),
+    tweet
+}
+
+tweets = [...tweets, tweet];
+
+tweetsHTML();
+formulario.reset();
+
+}
+
+function MostrarError(error){
+ const mesajeError = document.createElement('p');
+ mesajeError.textContent =error;
+ mesajeError.classList.add('error');
+
+const contenido = document.querySelector('#contenido');
+contenido.appendChild(mesajeError);
+
+setTimeout(() => {
+    mesajeError.remove();
+}, 3000);
+
+}
+
+function tweetsHTML(){
+clearHTML();
+    if(tweets.length > 0){
+        tweets.forEach(tweet => {
+         const li = document.createElement('li');
+         li.textContent = tweet;
+         lista_tweets.appendChild(li);
+        });
+    }
+        sincronizarStorage();
+}
+
+function sincronizarStorage() {
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+function clearHTML(){
+while (lista_tweets.firstChild) {
+    lista_tweets.removeChild(lista_tweets.firstChild);
+}
+}
+
