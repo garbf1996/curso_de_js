d7a92abb2aec7bb7b023b900c1ddecdd78683ca3
garbf1996 <garbfbatista@gmail.com>
2022-07-04 16:34:51 -0400

    pratica

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -3,7 +3,7 @@ const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 const formulario = document.querySelector('#enviar-mail'); 
-// 
+const vaciar = document.querySelector('#resetBtn');
 const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
@@ -16,8 +16,14 @@ function eventos() {
   asunto.addEventListener('blur',validarCampo);
   mensaje.addEventListener('blur',validarCampo);
   formulario.addEventListener('submit',emailEnviado);
+  vaciar.addEventListener('click', limpair);
+
 }
 
+function limpair(e){
+e.preventDefault();
+formulario.reset();
+}
 
 
 
