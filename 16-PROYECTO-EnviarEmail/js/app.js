f8bfff0967819498b92e5fa57d8e60775bb1186a
garbf1996 <garbfbatista@gmail.com>
2022-07-04 16:09:29 -0400

    praticando

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,96 +1,95 @@
-const btnEnviar = document.querySelector("#enviar");
-const email = document.querySelector("#email");
-const asunto = document.querySelector("#asunto");
-const mensaje = document.querySelector("#mensaje");
-const formulario = document.querySelector("#enviar-mail");
-const resetBtn = document.querySelector("#resetBtn");
+const btnEnviar = document.querySelector('#enviar');
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('#enviar-mail'); 
+// 
 const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-iniciarAPP();
-function iniciarAPP () {
-  document.addEventListener("DOMContentLoaded", bnt);
-  email.addEventListener("blur",input);
-  asunto.addEventListener("blur",input);
-  mensaje.addEventListener("blur",input);
-  resetBtn.addEventListener("click",limparFormulario);
-  formulario.addEventListener("submit",emailEnviado);
-}
 
-function bnt(){
-  btnEnviar.disabled = true;
-  btnEnviar.classList.add('cursor-not-allowed','opacity-50');
-}
-
-function limparFormulario(e){
-e.preventDefault();
-formulario.reset();
+eventos();
+function eventos() {
+  // Iniciar App
+  document.addEventListener('DOMContentLoaded',iniciarApp);
+// validar campo
+  email.addEventListener('blur',validarCampo);
+  asunto.addEventListener('blur',validarCampo);
+  mensaje.addEventListener('blur',validarCampo);
+  formulario.addEventListener('submit',emailEnviado);
 }
 
 
 
-function input(e){
 
-  if(e.target.value.length > 0){
+// Iniciar App
+function iniciarApp(){
+  //boton enviar desivilatado
+btnEnviar.disabled = true;
+btnEnviar.classList.add('cursor-not-allowed','opacity-50');
+}
 
-  const removeMesaje = document.querySelector('p.error');
+// fumcion para Validar Campo
+function validarCampo(e){
+  // validar campo si esta lleno
+if(e.target.value.length > 0){
 
-  if(removeMesaje){
-    removeMesaje.remove();
-  }
+const removeMesaje = document.querySelector('p.error');
+if(removeMesaje){
+  removeMesaje.remove();
+}
 
-    e.target.classList.remove('border','border-red-500');
-    e.target.classList.add('border','border-green-500');
-
-  }else{
-    e.target.classList.remove('border','border-green-500');
-    e.target.classList.add('border','border-red-500');
-    mensajeError("Todos los campos son obligatorios");
-  }
-
-  if(e.target.type == 'email'){
-
-    if(er.test(e.target.value)){
-
+  e.target.classList.remove('border-red-500');
+e.target.classList.add('border','border-green-500');
+}else{
+  // validar campo si esta vascio
+mensajeError('Todos los campos son obligatorios');
+e.target.classList.remove('border','border-green-500');
+e.target.classList.add('border','border-red-500');;
+}
+// validar email
+if(e.target.type == 'email'){
+if(er.test(e.target.value)){
 
   const removeMesaje = document.querySelector('p.error');
   if(removeMesaje){
     removeMesaje.remove();
   }
-
-    e.target.classList.remove('border','border-red-500');
-    e.target.classList.add('border','border-green-500');
-
   
-  }else{
+    e.target.classList.remove('border-red-500');
+  e.target.classList.add('border','border-green-500');
 
-    e.target.classList.remove('border','border-green-500');
-    e.target.classList.add('border','border-red-500');
-    mensajeError("Correo no es invalido");
- 
-  }
 
+}else{
+  mensajeError('Email no valido');
+  e.target.classList.remove('border','border-green-500');
+  e.target.classList.add('border','border-red-500');;
 }
-
+}
+// validar campo si esta lleno
 if(er.test(email.value) !=='' && asunto.value !== '' && mensaje.value !== ''){
   btnEnviar.disabled = false;
   btnEnviar.classList.remove('cursor-not-allowed','opacity-50');
-
-  }
 }
 
+}
+
+
+// funcion para mostrar mensaje de error
 function mensajeError(mesaje){
+// crear p
+  const mostrarMesaje = document.createElement('p');
+  // agregar texto
+  mostrarMesaje.textContent = mesaje;
+  // agregar clase
+  mostrarMesaje.classList.add('border','border-red-700','text-center','p-3','error','mt-2','text-red-700');
+  // insertarlo en el formulario
+  const ware = document.querySelectorAll('.error');
+if(ware.length == 0){
 
-  const mostrar = document.createElement('p');
-  mostrar.textContent = mesaje;
-  mostrar.classList.add('border','border-red-500','background-red-100','text-red-500','p-3','mt-5', 'error','text-center');
-   
-  const error = document.querySelectorAll('.error');
+  formulario.appendChild(mostrarMesaje);
 
-  if(error.length === 0){
+}
 
-formulario.appendChild(mostrar);
-
-  }
 }
 
 function emailEnviado(e){
@@ -99,7 +98,7 @@ function emailEnviado(e){
   const spinner = document.querySelector('#spinner');
 
   spinner.style.display = 'flex';
-
+//   spinner.style.display = 'none';
  setTimeout(() => {
 
   spinner.style.display = 'none';
@@ -107,7 +106,7 @@ function emailEnviado(e){
   correoEnaviado.textContent = 'Correo Enviado';
   correoEnaviado.classList.add('bg-green-500','text-white','border-b','font-bold','rounded-t','my-10','px-4','py-2','text-center');
   formulario.insertBefore(correoEnaviado,spinner);
-
+// limpiar formulario
 setTimeout(() => {
   correoEnaviado.remove();
   formulario.reset();
@@ -116,4 +115,8 @@ setTimeout(() => {
 }, 5000);  
  },3000);
 }
+
+
+
+
 
