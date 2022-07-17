a34b16330169677d270200fb2d07c2305ff0cb47
garbf1996 <garbfbatista@gmail.com>
2022-07-17 09:34:37 -0400

    insetar gatos en html

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -9,9 +9,38 @@ function eventListeners(){
 document.addEventListener('DOMContentLoaded',preguntarPresupuesto );
 }
 
+//Classes
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+};
+
+//Classes para interfaz
+class UI{
+insertarPresupuesto(cantidad){
+//Extraer el presupuesto de usuario
+const {presupuesto,restante} = cantidad;
+//Insertar el presupuesto en el HTML
+document.querySelector('#total').textContent = presupuesto;
+document.querySelector('#restante').textContent = restante;
+
+}    
+}
+
+
+//Distancia 
+const ui = new UI();
+let presupuesto;
+
 function preguntarPresupuesto(){
     const presupuestoUsuario = prompt('Ingrese su presupuesto');
     if(presupuestoUsuario === null || presupuestoUsuario === '' || isNaN(presupuestoUsuario)){
        window.location.reload();
     }
+    //Prosupuesto calido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+  ui.insertarPresupuesto(presupuesto)
 }
