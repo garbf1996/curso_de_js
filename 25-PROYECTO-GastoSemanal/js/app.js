5aa8b09a1abd8733ede2e4fc92b71abcbb72f220
garbf1996 <garbfbatista@gmail.com>
2022-07-17 10:20:38 -0400

    Mesaje de error

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -7,6 +7,7 @@ const gatosListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners(){
 document.addEventListener('DOMContentLoaded',preguntarPresupuesto );
+formulario.addEventListener('submit',agregarGasto);
 }
 
 //Classes
@@ -26,7 +27,23 @@ const {presupuesto,restante} = cantidad;
 //Insertar el presupuesto en el HTML
 document.querySelector('#total').textContent = presupuesto;
 document.querySelector('#restante').textContent = restante;
-
+}
+imprimirAlerta(mensaje,tipo){
+const divMensaje  = document.createElement('div');
+divMensaje.classList.add('text-center','alert');
+if(tipo === 'error'){
+    divMensaje.classList.add('alert-danger');
+}else{
+   divMensaje.classList.add('alert-success'); 
+}
+//Mensaje de error
+divMensaje.textContent = mensaje;
+//Insertar el mensaje en el HTML
+document.querySelector('.primario').insertBefore(divMensaje,formulario);
+//Eliminar el mensaje despues de 3 segundos
+setTimeout(() => {
+    divMensaje.remove();
+},3000);
 }    
 }
 
@@ -43,4 +60,26 @@ function preguntarPresupuesto(){
     //Prosupuesto calido
     presupuesto = new Presupuesto(presupuestoUsuario);
   ui.insertarPresupuesto(presupuesto)
+}
+
+//Agregar gastos
+function agregarGasto(e){
+    e.preventDefault();
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+
+    if(nombre === '' || cantidad === ''){
+                         //Mensaje de error                //Tipo de mensaje
+      ui.imprimirAlerta('Todos los campos son obligatorios','error');
+        return;
+    }else if(cantidad <= 0){
+                            //Mensaje de error               //Tipo de mensaje
+        ui.imprimirAlerta('La cantidad debe ser mayor a 0','error');
+        return;
+    }else if (isNaN(cantidad)){
+                            //Mensaje de error               //Tipo de mensaje
+        ui.imprimirAlerta('La cantidad debe ser un numero','error');
+        return;
+    }
+
 }
