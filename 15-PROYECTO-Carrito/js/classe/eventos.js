95422372a47b72f298f7e69d7d56e48d11d9e97d
garbf1996 <garbfbatista@gmail.com>
2022-07-27 13:15:45 -0400

    comentario

--- 15-PROYECTO-Carrito/js/classe/eventos.js
+++ 15-PROYECTO-Carrito/js/classe/eventos.js
@@ -1,12 +1,13 @@
-import {agregarCarrito} from './funciones.js';
-import {lista_cursos} from './selectores.js';
+import {agregarCarrito,borrarCurso,cleaCurso} from './funciones.js';
+import {lista_cursos,carrito,vaciar_carrito} from './selectores.js';
 class App{
     constructor(){
         this.initApp();
     }
     initApp(){
      lista_cursos.addEventListener('click',agregarCarrito);
-       
+     carrito.addEventListener('click',borrarCurso);
+     vaciar_carrito.addEventListener('click',cleaCurso);      
     }
 }
 
