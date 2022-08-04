d839ab36fcb2e1be08be89b3d810c59e7cbefc49
garbf1996 <garbfbatista@gmail.com>
2022-08-03 20:12:14 -0400

    clima

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -10,12 +10,12 @@ function buscarClima(e) {
 e.preventDefault();
 
 const ciudad = document.querySelector('#ciudad').value;
-const pais = document.querySelector('#pais').value;
-if(ciudad === '' || pais === ''){
+
+if(ciudad === ''){
     alert('Los campos son los obligatorios');
     return; 
 }
-API(ciudad, pais)
+API(ciudad)
 }
 
 function API(ciudad){
@@ -23,11 +23,32 @@ function API(ciudad){
     const API = '356b6a89b8055b578a3100eeaa8c940a';
      const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API}`;
 fetch(URL)
-.then((res) => res.json())
-.then(error =>{
-    if(error.cod === "404"){
-        alert('error');
-    }
-})
+    .then((res) => res.json())
+    .then(datos =>{
+      if(datos.cod === "404"){
+        alert("Error");
+      }
+      mostrarHTML(datos);
+    })
+}
+
+function mostrarHTML(datos) {
+
+const { main:{temp},sys:{country}} = datos;
+
+
+const temperature = temp - 273.15;
+
+console.log(datos);
+
+const datosInfor = document.createElement('p');
+datosInfor.classList.add('font-bold','text-6xl');
+datosInfor.innerHTML = `${parseInt(temperature, 10)} &#8451;`;
+
+const div = document.createElement('div');
+div.classList.add('text-center','text-white');
+div.appendChild(datosInfor);
+resultado.appendChild(div);
+
 
 }
