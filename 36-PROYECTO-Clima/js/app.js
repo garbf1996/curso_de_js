229be9b057cc7bd6362c9e8ce1b274785601c8ce
garbf1996 <garbfbatista@gmail.com>
2022-08-05 20:22:29 -0400

    buscar cacion

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -7,6 +7,7 @@ window.addEventListener('load', () => {
 });
 
 function buscarClima(e) {
+
 e.preventDefault();
 
 const ciudad = document.querySelector('#ciudad').value;
@@ -18,7 +19,9 @@ if(ciudad === ''){
 API(ciudad)
 }
 
+
 function API(ciudad){
+  limpiarHTML();
 
     const API = '356b6a89b8055b578a3100eeaa8c940a';
      const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API}`;
@@ -34,21 +37,49 @@ fetch(URL)
 
 function mostrarHTML(datos) {
 
-const { main:{temp},sys:{country}} = datos;
+const { main:{temp,temp_max,temp_min},name} = datos;
 
 
-const temperature = temp - 273.15;
+const temperature = kelvinToCelsius(temp);
+const temperatureMax = kelvinToCelsius(temp_max);
+const temperatureMin = kelvinToCelsius(temp_min);
 
 console.log(datos);
 
-const datosInfor = document.createElement('p');
-datosInfor.classList.add('font-bold','text-6xl');
-datosInfor.innerHTML = `${parseInt(temperature, 10)} &#8451;`;
+const datosname = document.createElement('p');
+datosname.innerHTML = `${name}`;
+datosname.classList.add('text-red-600','text-4xl','text-center','font-bold','my-5');
 
-const div = document.createElement('div');
-div.classList.add('text-center','text-white');
-div.appendChild(datosInfor);
-resultado.appendChild(div);
+
+
+const datosClima = document.createElement('p');
+datosClima.innerHTML = `${temperature} ºC`;
+datosClima.classList.add('text-white','text-6xl','text-center','font-bold','my-5');
+
+
+
+const datosMax = document.createElement('p');
+datosMax.innerHTML = `MAX: ${temperatureMax} ºC`;
+datosMax.classList.add('text-white','text-2xl','text-center','font-bold','my-5');
+
+
+const datosMin = document.createElement('p');
+datosMin.innerHTML = `Min: ${temperatureMin} ºC`;
+datosMin.classList.add('text-white','text-1xl','text-center','font-bold','my-5');
+
+resultado.appendChild(datosname);
+resultado.appendChild(datosClima);
+resultado.appendChild(datosMax);
+resultado.appendChild(datosMin);
 
 
 }
+
+const kelvinToCelsius = grados => {return parseInt (grados - 273.15);}
+
+function limpiarHTML() {
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
