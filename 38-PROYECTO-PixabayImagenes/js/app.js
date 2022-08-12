343c4855a38f9996ca30e7d02033ebdfd58460ad
garbf1996 <garbfbatista@gmail.com>
2022-08-11 20:20:41 -0400

    api cripto

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -1,107 +1,12 @@
-const resultado = document.querySelector('#resultado');
-const formulario = document.querySelector('#formulario');
 
-const registroPorpagina = 40;
-let totalPaginas;
-let iterador;
 
-window.onload =()=> {
-    formulario.addEventListener('submit', buscarImagenes);
+ document.addEventListener('DOMContentLoaded', cargando);
+
+function cargando() {
+ const url = 'uFPK7C1L5LUpYAbSDmvbGYnAWL0k4sAEeLpnixagTQgenozKj2cFM4yWhlFRZiVI';
+
+ fetch(url).then((res)=>res.json)
+  .then((data)=>{
+    console.log(data);
+  })
 }
-
-function buscarImagenes(e){
-e.preventDefault();
-
-const busquedaTermino = document.querySelector('#termino').value;
-
-if(busquedaTermino === ''){
-   
-
-    Swal.fire({
-        icon: 'error',
-        title: 'Error...',
-        text: 'Ingrese un termino de busqueda!',
-      })
-      return;
-}
-busquedaTerminoAPI(busquedaTermino);
-}
-
-function busquedaTerminoAPI(terminos){
-
-    const key = '29103166-1f042be62654c17f74038ac97';
-    const url = `https://pixabay.com/api/?key=${key}&q=${terminos}&image_type=photo&per_page=100`;
-    
-    fetch(url)
-    .then(res => res.json())
-
-    .then(datos => {
-      if(`${datos.totalHits}` === '0'){
-        Swal.fire({
-            icon: 'error',
-            title: 'Error...',
-            text: 'No se encontraron resultados!',
-          })
-          return;
-      }
-      imagHTML(datos.hits);
-      totalPaginas = calculaPaginas(datos.totalHits);
-      console.log(totalPaginas);
-    })
-    
-}
-
-
-function imagHTML(images){
-limpiaHTML();
- images.forEach(image => {
-  const {likes,downloads,largeImageURL,previewURL} = image;
-
-resultado.innerHTML += `
-<div class="w-1/2 md:w-1/3 lg:w-1/4 mb-4 p-3">
-<div class="bg-white">
-<img src="${previewURL}" class="w-full" alt="">
-<div class="p-2">
-<p class="font-bold">${likes}<span class="font-light"> Me gustas</span></p>
-</div>
-
-<div class="p-2">
-<p class="font-bold">${downloads}<span class="font-light"> Descargar</span></p>
-</div>
-<div class="p-2">
-<a href="${largeImageURL}"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center mt-5 p-1 w-full">Ver image</button></a>
-</div>
-</div>
-</div>
-`;
- })  
- mostrarPaginas(images);
-}
-
-function limpiaHTML(){
-   while(resultado.firstChild){
-    resultado.removeChild(resultado.firstChild);
-   }
-}
-
-function calculaPaginas(total){
-return parseInt(Math.ceil(total/registroPorpagina));
-
-}
-
-function *crearPaginas(total){
-
-  for(let i = 1; i <= total; i++){
-    yield i;
-  }
-  
-}
-
-
-function mostrarPaginas(){
-  iterador = crearPaginas(totalPaginas);
-  console.log(iterador.next().done);
-}
-
-
-
---
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -0,0 +1,126 @@
+const criptomoedaSelect = document.querySelector('#criptomonedas');
+const selectmoneda = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+const objBuscador = {
+    moneda: '',
+    criptomoneda: ''
+}
+
+const obterCriptomoedas = criptomoedas => new Promise((resolve) => {
+    resolve(criptomoedas);
+})
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+consultarCriptomoedas();
+formulario.addEventListener('submit', obtenerValores);
+criptomoedaSelect.addEventListener('change', leerValores);
+selectmoneda.addEventListener('change', leerValores);
+});
+
+function consultarCriptomoedas(){
+   const url = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=25&tsym=USD';
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => 
+        obterCriptomoedas(resultado.Data))
+    .then(criptomoedas => selectCriptomonedas(criptomoedas))
+    
+}
+
+function selectCriptomonedas(criptomoedas){
+    criptomoedas.forEach(criptomoeda => {
+        const option = document.createElement('option');
+        option.value = criptomoeda.CoinInfo.Name;
+        option.textContent = criptomoeda.CoinInfo.FullName;
+        criptomoedaSelect.appendChild(option);  
+       ;
+    } )
+}
+
+function leerValores(e){
+e.preventDefault();
+objBuscador[e.target.name] = e.target.value;
+
+console.log(objBuscador);
+}
+
+function obtenerValores(e){
+    e.preventDefault();
+    const {moneda, criptomoneda} = objBuscador;
+    if(moneda === '' || criptomoneda === ''){
+        Swal.fire({
+            icon: 'error',
+            title: 'Todos los campos son obligatorios',
+
+            
+          })
+        return;
+    }
+    consultarAPI();
+}
+
+function consultarAPI(){
+    const {moneda, criptomoneda} = objBuscador;
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+     mostrarSpiner();
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(cotizacion => {
+        console.log(cotizacion);
+        mostrarResultado(cotizacion.DISPLAY[criptomoneda][moneda]);
+     ;
+    })
+
+}
+
+function mostrarResultado(cotizacion){
+    limpiarResultado();
+
+ const {PRICE, HIGHDAY,LOWDAY,CHANGEPCT24HOUR,LASTUPDATE} = cotizacion;
+ const precio = document.createElement('p');
+ precio.classList.add('precio');
+    precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+    resultado.appendChild(precio);
+
+    const precioAlto = document.createElement('p');
+    precioAlto.innerHTML = `El precio más alto del día es: <span>${HIGHDAY}</span>`;
+    resultado.appendChild(precioAlto);
+
+    const precioBajo = document.createElement('p');
+    precioBajo.innerHTML = `El precio más bajo del día: <span>${LOWDAY}</span>`;
+    resultado.appendChild(precioBajo);
+
+    const ultimaHORA = document.createElement('p');
+    ultimaHORA.innerHTML = `Ultima 24 horas: <span>${CHANGEPCT24HOUR}%</span>`;
+    resultado.appendChild(ultimaHORA);
+
+    const ultimaActualizacion = document.createElement('p');
+    ultimaActualizacion.innerHTML = `Ultima actualizacion: <span>${LASTUPDATE}</span>`;
+    resultado.appendChild(ultimaActualizacion);
+
+}
+
+function limpiarResultado(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function mostrarSpiner(){
+    limpiarResultado();
+    const spiner = document.createElement('div');
+    spiner.classList.add('spinner');
+
+    spiner.innerHTML = `
+  <div class="sk-chase-dot"></div>
+  <div class="sk-chase-dot"></div>
+  <div class="sk-chase-dot"></div>
+    `;
+resultado.appendChild(spiner);
+}
