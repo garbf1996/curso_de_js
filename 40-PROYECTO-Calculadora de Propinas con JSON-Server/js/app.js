afa4ae2d8ec26244d6a91c1b77e1c1c04fe97751
garbf1996 <garbfbatista@gmail.com>
2022-08-16 18:26:33 -0400

    validar campos

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -10,9 +10,20 @@ btnGuadarCliente.addEventListener("click",guadarCliente);
 
 function guadarCliente(e){
     e.preventDefault();
-    cliente.mesa = document.querySelector("#mesa").value;
-    cliente.hora = document.querySelector("#hora").value;
-    cliente.pedidos = document.querySelector("#pedidos").value;
-   
+ const mesa = document.querySelector("#mesa").value;
+ const hora = document.querySelector("#hora").value;
+
+ const campoVacio = [mesa, hora].some(campo => campo === "");
+
+    if(campoVacio){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Todos los campos son obligatorios!',
+            footer: '<a href="">Why do I have this issue?</a>'
+          })
+          return;
+    }
+
 
 }
