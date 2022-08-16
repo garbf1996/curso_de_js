c35515578bb09fc4210b9ec34e071cf7c2f8ea69
garbf1996 <garbfbatista@gmail.com>
2022-08-16 19:22:52 -0400

    api

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -1,18 +1,19 @@
+const contenidoPlatillos = document.querySelector(".contenido");
 let cliente = {
     mesa: "",
     hora: "",
     pedidos: []
 }
-
+//Agregar pedido
 const btnGuadarCliente = document.querySelector("#guardar-cliente");
 
 btnGuadarCliente.addEventListener("click",guadarCliente); 
-
+//validar que los campos no esten vacios
 function guadarCliente(e){
     e.preventDefault();
  const mesa = document.querySelector("#mesa").value;
  const hora = document.querySelector("#hora").value;
-
+//validar que los campos no esten vacios
  const campoVacio = [mesa, hora].some(campo => campo === "");
 
     if(campoVacio){
@@ -24,6 +25,49 @@ function guadarCliente(e){
           })
           return;
     }
+    //Asignar valores al objeto cliente
+  cliente = {
+    ...cliente,
+    mesa,
+    hora,
+  }
+  //Cerrar modal
+   const modalFormulario = document.querySelector("#formulario");
+   const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+ mostraSeccion();
+ getPlatillos();
+}
 
+function mostraSeccion(){
+    const seccion = document.querySelectorAll(".d-none");
+    seccion.forEach(seccion => seccion.classList.remove("d-none"));
+}
+
+function getPlatillos(){
+    const url = "http://localhost:4000/platillos";
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => getAPI(data))
+    .then(error => console.log(error))
+}
+
+function getAPI(platillos){
+const contenedorPlatillos = document.querySelector("#platillos");
+    platillos.forEach(platillo => {
+        const {nombre, precio, descripcion} = platillo;
+        contenedorPlatillos.innerHTML += `
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">${nombre}</h5>
+                <p class="card-text">${descripcion}</p>
+                <h5 class="card-title">$${precio}</h5>
+                <button class="btn btn-primary agregar-platillo" data-id="${platillo.id}">Agregar</button>
+            </div>
+        </div>
+        `;
+        contenidoPlatillos.appendChild(contenedorPlatillos);
+    });
 
 }
