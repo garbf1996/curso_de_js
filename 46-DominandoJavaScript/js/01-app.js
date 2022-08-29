1ebf6925a0345e8b66b74c09399e62f623730089
garbf1996 <garbfbatista@gmail.com>
2022-08-29 14:09:05 -0400

    hoisting

---
+++ 46-DominandoJavaScript/js/01-app.js
@@ -0,0 +1,13 @@
+//Scope
+const login = truer;
+const cliente = 'Garbr';
+function clienteLogueaso (){
+
+}
+
+function function2(){
+    console.log(cliente);
+}
+
+function2();
+clienteLogueaso();
---
+++ 46-DominandoJavaScript/js/02-app.js
@@ -0,0 +1,6 @@
+//hoisting
+obtenerCliente('Garber')
+function obtenerCliente(Nombre){
+    console.log(`El nombre del cliente: ${Nombre}`);
+}
+ 
